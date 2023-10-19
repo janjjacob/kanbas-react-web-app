fec3f888503f24de7863705ed390f90ce7e1caf5
janjjacob <janjjacob@gmail.com>
2023-10-19 18:00:44 -0400

    changed navBar icons

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FaUser, FaBook, FaCalendar, FaInbox, FaClock, FaTv, FaQuestionCircle } from 'react-icons/fa';
+import { FaUser, FaBook, FaCalendar, FaInbox, FaClock, FaTv, FaQuestionCircle, FaTachometerAlt, FaArrowAltCircleRight } from 'react-icons/fa';
 
 import './index.css';
 function KanbasNavigation() {
     const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
-    const icons = [FaUser, FaBook, FaCalendar, FaInbox, FaClock, FaTv, FaQuestionCircle];
+    const icons = [FaUser, FaTachometerAlt, FaBook, FaCalendar, FaInbox, FaClock, FaTv, FaArrowAltCircleRight, FaQuestionCircle];
     const { pathname } = useLocation();
     return (
         <div className="list-group text-white wd-kanbas-navigator wd-pos-fixed-navigator wd-sidebar-font text-center" style={{ width: 150, backgroundColor: 'black' }}>
